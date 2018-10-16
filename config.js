e762cd2c3b6ba1748cdda2ed6c2f495043567714
Aaron Whitehead <whitehead.aaron@live.com>
2018-10-16 11:55:09 -0700

    test

--- config.js
+++ config.js
@@ -3,8 +3,8 @@
 module.exports = {
   PORT: process.env.PORT || 8080,
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
-  MONGODB_URI:
-        process.env.MONGODB_URI || 'mongodb://localhost/thinkful-backend',
+  DATABASE_URL:
+        process.env.DATABASE_URL || 'mongodb://localhost/thinkful-backend',
   TEST_DATABASE_URL:
         process.env.TEST_DATABASE_URL ||
         'mongodb://localhost/thinkful-backend-test'
