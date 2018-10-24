00c958a099e427a56f60cbc2d5518a82fcfb43b3
Aaron Whitehead <whitehead.aaron@live.com>
2018-10-24 10:23:00 -0700

    updates

--- passport/local.js
+++ passport/local.js
@@ -1,7 +1,6 @@
 const { Strategy: LocalStrategy } = require('passport-local');
 const User = require('../models/user');
 
-
 const localStrategy = new LocalStrategy((username, password, done) => {
   let user;
   User.findOne({ username })
